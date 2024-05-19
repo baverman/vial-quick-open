f4efacae2c1b11888d321eb5e6967b02e61c8ad7
Anton Bobrov <baverman@gmail.com>
2024-05-19 15:40:55 +0100

    python3 support

--- vial-plugin/quick_open/__init__.py
+++ vial-plugin/quick_open/__init__.py
@@ -4,7 +4,7 @@ def init():
     vial.register_command('VialQuickOpen', '.plugin.quick_open')
 
     vial.vim.vars['vial_ignore_dirs_default'] = ['^build$', '^dist$',
-        '(^|.*/)__pycache__$', '.*\.egg-info$', '(^|.*/)\.']
+        '(^|.*/)__pycache__$', r'.*\.egg-info$', r'(^|.*/)\.']
 
     vial.vim.vars['vial_ignore_extensions_default'] = [
         'pyc', 'pyo', 'swp', 'class', 'o']
